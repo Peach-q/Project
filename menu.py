1cce878b36f53992f923fbd0c1be686d6fa8fc10
Peach-q <72335512+Peach-q@users.noreply.github.com>
2021-12-13 22:28:20 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,9 +1,7 @@
 import pygame
 import sys
-
 class Menu():
     back_game = pygame.image.load('images/space.png')
-
     def __init__(self, screen):
         self.screen = screen
 
@@ -16,31 +14,29 @@ class Menu():
 
         main_screen_word = main_font.render("Space rangers", 70, (255, 255, 255))
         self.screen.blit(main_screen_word, (55, 100))
-
-        if 140 < pygame.mouse.get_pos()[0] < 460 and 380 < pygame.mouse.get_pos()[1] < 440:
-            screen_word = font.render("Новая игра", True, (128, 0, 128))
+        if 150 < pygame.mouse.get_pos()[0] < 470 and 390 < pygame.mouse.get_pos()[1] < 450:
+            text = font.render("Новая игра", True, (128, 0, 128))
         else:
-            screen_word = font.render("Новая игра", True, (255, 255, 255))
-        self.screen.blit(screen_word, (175, 380))
-
-        if 180 < pygame.mouse.get_pos()[0] < 420 and 460 < pygame.mouse.get_pos()[1] < 520:
-            screen_word = font.render("Выйти из игры", True, (128, 0, 128))
+            text = font.render("Новая игра", True, (255, 255, 255))
+        self.screen.blit(text, (140, 380))
+        if 200 < pygame.mouse.get_pos()[0] < 410 and 470 < pygame.mouse.get_pos()[1] < 530:
+            text = font.render("Выход", True, (128, 0, 128))
         else:
-            screen_word = font.render("Выйти из игры", True, (255, 255, 255))
-        self.screen.blit(screen_word, (140, 500))
-
+            text = font.render("Выход", True, (255, 255, 255))
+        self.screen.blit(text, (200, 470))
         self.screen.blit(pygame.image.load('images/pixil-frame-0.png'), (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]))
         pygame.display.flip()
         pygame.display.update()
 
+
     def selection_from_the_menu(self):
 
-        for game_event in pygame.event.get():
-            if game_event.type == pygame.QUIT:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
                 sys.exit()
-            if game_event.type == pygame.MOUSEBUTTONDOWN and game_event.button == 1:
-                if 30 < pygame.mouse.get_pos()[0] < 400 and 400 < pygame.mouse.get_pos()[1] < 660:
+            if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+                if 200 < pygame.mouse.get_pos()[0] < 400 and 460 < pygame.mouse.get_pos()[1] < 520:
                     sys.exit()
-                return 100 < pygame.mouse.get_pos()[0] < 600 and 380 < pygame.mouse.get_pos()[1] < 440
-
+                return 150 < pygame.mouse.get_pos()[0] < 470 and 390 < pygame.mouse.get_pos()[1] < 450
+            
         return False
