0ad70df715cf82cd58ba30392e2174f940499047
Peach-q <72335512+Peach-q@users.noreply.github.com>
2021-12-13 20:54:45 +0300

    Update Space_Game.py

--- Space_Game.py
+++ Space_Game.py
@@ -3,6 +3,9 @@ from gun import Gun
 from pygame.sprite import Group
 from stats import Stats
 from scores import Scores
+from menu import Menu
+
+
 
 def run():
 
@@ -10,9 +13,12 @@ def run():
     screen = pygame.display.set_mode((700, 700))
     pygame.display.set_caption('Space rangers (Zenkevich home-project)')
     bg_color = (0, 0, 0)
+
     pygame.mixer.music.load('sound/сhill.wav')
-    pygame.mixer.music.set_volume(0.4)           
+    pygame.mixer.music.set_volume(0.4)
     pygame.mixer.music.play(-1)
+
+    menu = Menu(screen)
     gun = Gun(screen)
     bullets = Group()
     inos = Group()
@@ -20,13 +26,17 @@ def run():
     stats = Stats()
     sc = Scores(screen, stats)
 
+
     while True:
-        controls.events(screen, gun, bullets)
         if stats.run_game:
+            controls.events(screen, gun, bullets)
             gun.update_gun()
             bullets.update()
             controls.update(bg_color, screen, stats, sc, gun, inos, bullets)
             controls.update_bullets(screen, stats, sc, inos, bullets)
             controls.update_inos(stats, screen, sc, gun, inos, bullets)
+        else:
+            menu.update_menu()
+            stats.run_game = menu.make_choice()
 
 run()
